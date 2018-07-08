3092196cd0ac20b5329930aed5be63cffb157f2f
Pan Cheng <pchengeducation@hotmail.com>
2018-07-08 05:36:18 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,8 @@
 # Add  code here!
 def prime?(num)
-  (2..Math.sqrt(num)).each { |i|
+  (2..Math.sqrt(num)).each |i|
     if num % i == 0 && i < num
       return false
-    end
-    }
-    true
+    else true
+end
 end
